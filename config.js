1343bc0a26362c64d852d02dd374097f3eaa150d
lucadir74-ai <lucadir74@gmail.com>
2026-08-17 01:40:04 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 window.CONFIG = {
   url:    "https://uqaddhbayyhudbmqixic.supabase.co",
-  chiave: "sb_publishable_3bPeLidM3lI97ruAadBqRg_m9ozR61S
-"
+  chiave: "sb_publishable_3bPeLidM3lI97ruAadBqRg_m9ozR61S"
 };
